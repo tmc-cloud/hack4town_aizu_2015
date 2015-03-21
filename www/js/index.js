1fc167e7b3d790d00fa5aae7df0723c94b630889
asato <akihiko-sato@tmc.co.jp>
2015-03-22 01:59:57 +0900

    ダイアログ周りを作成

--- www/js/index.js
+++ www/js/index.js
@@ -6,3 +6,21 @@ $(document).ready(function(){
     $("#radar_body").css({"height":viewwidth*0.9, "top":v_middle, "left":(viewwidth * 0.05), "border-radius":viewwidth/2});
 
 });
+
+setTimeout(findBeacon, 1500);
+
+function findBeacon(uuid, major, minor, proximity){
+    $("#search").css({"-webkit-filter":"blur(10px)"});
+    $("#find_box").css({"display":"block"});
+}
+
+function showDetail(){
+    $("#find_box").css({"display":"none"});
+    $("#detail_box").css({"display":"block"});
+}
+
+function hideDialog(){
+    $("#find_box").css({"display":"none"});
+    $("#detail_box").css({"display":"none"});
+    $("#search").css({"-webkit-filter":""});
+}
