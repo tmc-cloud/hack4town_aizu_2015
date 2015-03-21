1725487ad678b6659b6d88bcf619badc27386115
asato <akihiko-sato@tmc.co.jp>
2015-03-21 23:40:03 +0900

    レイアウト微調整

--- www/js/index.js
+++ www/js/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     var viewheight = $(document).height();
     var viewwidth = $(document).width();
-    var v_middle = (viewheight/2) - (viewwidth / 2);
+    var v_middle = (viewheight/2) - (viewwidth *0.9/ 2);
 
-    $("#radar_body").css({"height":viewwidth, "top":v_middle, "border-radius":viewwidth/2});
+    $("#radar_body").css({"height":viewwidth*0.9, "top":v_middle, "left":(viewwidth * 0.05), "border-radius":viewwidth/2});
 
 });
